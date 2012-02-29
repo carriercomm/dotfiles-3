4bc867580f8f5b01087b3de76414134ce7fd385d
Josh Lapham <josh@joshlapham.com>
2012-03-01 00:01:24 +1100

    Update to history file; added improved completion. Misc editing on other files.

--- zsh/misc.zsh
+++ zsh/misc.zsh
@@ -1,23 +1,2 @@
-# Push and pop directories on directory stack
-alias pu='pushd'
-alias po='popd'
-
-# Basic directory operations
-alias ..='cd ..'
-alias ...='cd ../..'
-alias -- -='cd -'
-
-# List direcory contents
-alias lsa='ls -lah'
-alias l='ls -lahG'
-alias ll='ls -l'
-
-# Safety with rm
+# Safety first with rm
 alias rm='rm -i'
-
-# Add to Instapaper
-alias insta='python ~/.dotfiles/bin/instacli.py'
-
-# Funkiller - disable time-wasting websites
-alias funkiller='sudo bash ~/.dotfiles/bin/funkiller'
-
